ec4ca9f89eba348e36ef3720fd0a7fa684c65419
saturnua <saturnua@gmail.com>
2019-03-17 22:07:36 +0200

    fix data in sql

--- src/main/resources/db/migration/V1_5__createDemoLectures.sql
+++ src/main/resources/db/migration/V1_5__createDemoLectures.sql
@@ -1,5 +1,5 @@
-INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (1, '2018-01-01 12:00:00','Math', 1, 1, 22);
-INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (2, '2018-02-01 12:00:00','Biology', 2, 2, 23);
-INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (3, '2018-03-01 12:00:00','Chemistry', 3, 3, 24);
-INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (4, '2018-04-01 12:00:00','Physics', 4, 1, 25);
+INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (1, '2019-03-01 12:00:00','Math', 1, 1, 22);
+INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (2, '2019-03-01 12:00:00','Biology', 2, 2, 23);
+INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (3, '2019-03-01 12:00:00','Chemistry', 3, 3, 24);
+INSERT into lectures(id, date, subject, teacher_id, group_id, classroom) VALUES (4, '2019-03-01 12:00:00','Physics', 4, 1, 25);
 

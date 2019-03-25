5d4d8ddff4e8e59455a6026ce71a2b0cf94658ea
saturnua <saturnua@gmail.com>
2019-03-25 22:13:42 +0200

    Fix initdb.sql and initdb.bat

--- database/initdb.bat
+++ database/initdb.bat
@@ -2,4 +2,4 @@
 SET PGOPTIONS=-c client_min_messages=error
 SET PGPASSWORD=postgres
 chcp 1251
-psql -h localhost -p 5433 -d test -U university_app -w -f .\initdb.sql
+psql -h localhost -p 5433 -U postgres -w -f .\initdb.sql
